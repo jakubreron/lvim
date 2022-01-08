86c4d27d22e0b3184922182b4b56bacc572c8b37
Jakub Reroń <kubareron@gmail.com>
2022-01-08 22:11:26 +0100

    first commit

---
+++ config.lua
@@ -0,0 +1,13 @@
+require("config.general")
+
+require("config.themes")
+
+require("config.keymaps")
+
+require("config.builtin")
+
+require("config.lsp")
+
+require("config.autocommands")
+
+require("config.plugins")
