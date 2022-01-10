42390143462551b2625f264e0ba6f56f1e13fee2
Jakub Reroń <kubareron@gmail.com>
2022-01-10 17:03:54 +0100

    config(lsp): ⚙️ auto-commit changes

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,7 +4,7 @@ lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.document_highlight = true
 lvim.lsp.code_lens_refresh = true
 
--- require("lvim.lsp.manager").setup("emmet_ls")
+require("lvim.lsp.manager").setup("emmet_ls")
 -- require("lvim.lsp.manager").setup("tailwindcss")
 -- require("lvim.lsp.manager").setup("volar")
 
