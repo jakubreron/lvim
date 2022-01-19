38d8820de6e0ff6c7d9415f31286ca1d22c343d2
Jakub Reroń <kubareron@gmail.com>
2022-01-19 10:32:30 +0100

    config(themes): ⚙️ auto-commit changes

--- lua/config/themes.lua
+++ lua/config/themes.lua
@@ -2,6 +2,6 @@
 lvim.colorscheme = "tokyonight"
 -- lvim.transparent_window = true
 
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_style = "night"
