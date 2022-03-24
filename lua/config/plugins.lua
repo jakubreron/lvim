f9233d40d782f6d09d49de266fe4d8140fd3b9ad
Jakub Reroń <kubareron@gmail.com>
2022-03-24 22:28:16 +0100

    config(plugins): ⚙️ auto-commit changes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -102,7 +102,22 @@ lvim.plugins = {
     "vuki656/package-info.nvim", -- check if the package info is up to date
     requires = "MunifTanjim/nui.nvim",
     config = function()
-      require "plugin.package-info"
+      require("package-info").setup {
+        {
+          colors = {
+            up_to_date = "#3C4048", -- Text color for up to date package virtual text
+            outdated = "#6ec0fa", -- Text color for outdated package virtual text
+          },
+          icons = {
+            enable = true, -- Whether to display icons
+            style = {
+              up_to_date = "|  ", -- Icon for up to date packages
+              outdated = "|  ", -- Icon for outdated packages
+            },
+          },
+          autostart = true, -- Whether to autostart when `package.json` is opened
+        },
+      }
     end,
     ft = "json",
   },
