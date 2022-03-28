c7c21b0c7d0d35615cbfcf506f53ff530d242a0b
Jakub Reroń <kubareron@gmail.com>
2022-03-28 10:35:43 +0200

    config(lsp): ⚙️ auto-commit changes

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,66 +7,69 @@ lvim.lsp.code_lens_refresh = true
 lvim.lsp.automatic_servers_installation = true
 
 -- NOTE: volar (vue 3) is the default, enable vuels to use Vue 2
-require("lvim.lsp.manager").setup("vuels")
--- NOTE: disallow volar 
+-- require("lvim.lsp.manager").setup "vuels"
 vim.list_extend(lvim.lsp.override, { "volar" })
 
 local allowed_servers = { "angularls", "emmet_ls", "tailwindcss" }
-lvim.lsp.override = vim.tbl_filter(function(name)
-	return not vim.tbl_contains(allowed_servers, name)
+lvim.lsp.override = vim.tbl_filter(function(server)
+  return not vim.tbl_contains(allowed_servers, server)
+end, lvim.lsp.override)
+
+lvim.lsp.override = vim.tbl_filter(function(server)
+  return server ~= "vuels"
 end, lvim.lsp.override)
 
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 lvim.lsp.on_attach_callback = function(client, bufnr)
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-	--Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	if client.name == "tsserver" or client.name == "jsonls" then
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
-	end
+  if client.name == "tsserver" or client.name == "jsonls" then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end
 end
 
-local formatters = require("lvim.lsp.null-ls.formatters")
-local linters = require("lvim.lsp.null-ls.linters")
+local formatters = require "lvim.lsp.null-ls.formatters"
+local linters = require "lvim.lsp.null-ls.linters"
 
 local filetypes = {
-	eslint = {
-		"javascript",
-		"javascriptreact",
-		"vue",
-		"typescript",
-		"typescriptreact",
-	},
-	stylelint = {
-		"css",
-		"scss",
-		"less",
-		"sass",
-	},
-	prettier = {
-		"html",
-		"markdown",
-		"yaml",
-		"json",
-	},
+  eslint = {
+    "javascript",
+    "javascriptreact",
+    "vue",
+    "typescript",
+    "typescriptreact",
+  },
+  stylelint = {
+    "css",
+    "scss",
+    "less",
+    "sass",
+  },
+  prettier = {
+    "html",
+    "markdown",
+    "yaml",
+    "json",
+  },
 }
 
-formatters.setup({
-	{ exe = "eslint_d", filetypes = filetypes.eslint },
-	-- { exe = "prettierd", filetypes = filetypes.eslint },
-	{ exe = "stylelint", filetypes = filetypes.stylelint },
-	{ exe = "prettierd", filetypes = filetypes.prettier },
-	{ exe = "stylua", filetypes = { "lua" } },
-})
+formatters.setup {
+  { exe = "eslint_d", filetypes = filetypes.eslint },
+  -- { exe = "prettierd", filetypes = filetypes.eslint },
+  { exe = "stylelint", filetypes = filetypes.stylelint },
+  { exe = "prettierd", filetypes = filetypes.prettier },
+  { exe = "stylua", filetypes = { "lua" } },
+}
 
-linters.setup({
-	{ exe = "eslint_d", filetypes = filetypes.eslint },
-	{ exe = "stylelint", filetypes = filetypes.stylelint },
-	{ exe = "luacheck", filetypes = { "lua" } },
-})
+linters.setup {
+  { exe = "eslint_d", filetypes = filetypes.eslint },
+  { exe = "stylelint", filetypes = filetypes.stylelint },
+  { exe = "luacheck", filetypes = { "lua" } },
+}
