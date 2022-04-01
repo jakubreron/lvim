771b4df045ea7a337314d01b0652783dc56181a5
Jakub Reroń <kubareron@gmail.com>
2022-04-01 19:10:07 +0200

    cleanup: remove indent blankline plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -40,18 +40,6 @@ lvim.plugins = {
     end,
   },
   {
-    "lukas-reineke/indent-blankline.nvim", -- | characters on blank lines
-    event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
-    end,
-  },
-  {
     "ruifm/gitlinker.nvim", -- quick link to the git snippet
     event = "BufRead",
     config = function()
