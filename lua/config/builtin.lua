f53329e0ef8d72d4759c44a60e88e0c7bb5a4ba8
Jakub Reroń <kubareron@gmail.com>
2022-04-01 19:57:33 +0200

    config(themes): ⚙️ auto-commit changes

--- lua/config/builtin.lua
+++ lua/config/builtin.lua
@@ -29,8 +29,6 @@ lvim.builtin.lualine.sections = {
 
 lvim.builtin.lualine.inactive_sections.lualine_a = { components.filename }
 
-lvim.builtin.cmp.experimental.ghost_text = true
-
 vim.g.bufferline = {
   auto_hide = true,
   animation = false,
