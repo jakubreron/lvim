28e20e3aa835c1d586a0d7c8b179eddd3ae856e3
Jakub Reroń <kubareron@gmail.com>
2022-04-21 11:29:52 +0200

    config(lsp): ⚙️ auto-commit changes

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,14 +6,6 @@ lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(serve
   return not vim.tbl_contains(allowed_servers, server)
 end, lvim.lsp.automatic_configuration.skipped_servers)
 
--- enable vuels (vue 2)
-lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-  return server ~= "vuels"
-end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- disable volar (vue 3)
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "volar" })
-
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
