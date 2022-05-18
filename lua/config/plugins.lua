b0a2db298784bce894fe61baefcfbac2b38a8126
Jakub Reroń <kubareron@gmail.com>
2022-05-18 09:46:24 +0200

    config(plugins): ⚙️ auto-commit changes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -65,32 +65,32 @@ lvim.plugins = {
     end,
     ft = "json",
   },
-  -- {
-  --   "kevinhwang91/nvim-bqf", -- better quickfix window (preview, search & replace, etc...)
-  --   event = { "BufRead", "BufNew" },
-  --   config = function()
-  --     require("bqf").setup {
-  --       auto_enable = true,
-  --       preview = {
-  --         win_height = 12,
-  --         win_vheight = 12,
-  --         delay_syntax = 80,
-  --         border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
-  --       },
-  --       func_map = {
-  --         vsplit = "",
-  --         ptogglemode = "z,",
-  --         stoggleup = "",
-  --       },
-  --       filter = {
-  --         fzf = {
-  --           action_for = { ["ctrl-s"] = "split" },
-  --           extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
+  {
+    "kevinhwang91/nvim-bqf", -- better quickfix window (preview, search & replace, etc...)
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup {
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      }
+    end,
+  },
   {
     "Pocco81/AutoSave.nvim",
     config = function()
