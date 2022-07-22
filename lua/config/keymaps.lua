5bf9da896af0f5128cbb825c573b545be5109b40
Jakub Reroń <kubareron@gmail.com>
2022-07-22 13:18:35 +0200

    config(keymaps): ⚙️ auto-commit changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,7 +16,7 @@ lvim.keys = {
 }
 
 lvim.builtin.which_key.mappings["q"] = { ":qa<CR>", "Quit" }
-lvim.builtin.which_key.mappings["e"] = { "<cmd>Ex<CR>", "Explorer" }
+-- lvim.builtin.which_key.mappings["e"] = { "<cmd>Ex<CR>", "Explorer" }
 
 lvim.builtin.which_key.mappings["T"] = nil -- treesitter
 lvim.builtin.which_key.mappings["f"] = nil -- find file (already binded to sf)
