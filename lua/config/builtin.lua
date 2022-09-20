6d385d1c4045755c1d3c7660980c3b7d79df80ee
Jakub Reroń <kubareron@gmail.com>
2022-09-20 23:11:14 +0200

    config(builtin): ⚙️ auto-commit changes

--- lua/config/builtin.lua
+++ lua/config/builtin.lua
@@ -1,12 +1,10 @@
 lvim.builtin.terminal.active = true
--- lvim.builtin.notify.active = true
--- lvim.builtin.nvimtree.active = false
-lvim.builtin.nvimtree.setup.disable_netrw = false
+-- lvim.builtin.nvimtree.setup.disable_netrw = false
 
 lvim.builtin.bufferline.options.numbers = "buffer_id"
 lvim.builtin.bufferline.options.always_show_bufferline = true
 
--- lvim.builtin.project.silent_chdir = false
+lvim.builtin.project.silent_chdir = false
 
 local components = require "lvim.core.lualine.components"
 components.filename = {
@@ -21,18 +19,13 @@ local function packageinfo()
 end
 
 -- lvim.builtin.lualine.options.globalstatus = true
--- lvim.builtin.lualine.sections = {
---   lualine_c = {
---     components.diff,
---     components.python_env,
---     packageinfo,
---   },
---   -- lualine_z = { "location" }
--- }
-
+lvim.builtin.lualine.sections.lualine_a = {
+  components.diff,
+  components.python_env,
+  packageinfo,
+}
 lvim.builtin.lualine.inactive_sections.lualine_a = { components.filename }
 
--- default settings from the repo
 -- TODO: remove phpdoc from ignored after they fix the upstream: https://github.com/nvim-treesitter/nvim-treesitter/issues/2837
 lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.treesitter.ignore_install = { "phpdoc" }
