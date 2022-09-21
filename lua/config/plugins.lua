f79331a2fc9521a1f0efe050d8db0a98ee55fc7d
Jakub Reroń <kubareron@gmail.com>
2022-09-21 09:56:57 +0200

    config(plugins): ⚙️ auto-commit changes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -2,32 +2,28 @@ vim.g.vimwiki_list = { { path = "~/Documents/vimwiki/", syntax = "markdown", ext
 vim.g["test#strategy"] = "dispatch" -- make test commands execute using dispatch.vim
 vim.b["match_words"] = "<<<<<<<:=======:>>>>>>>" -- match git conflict markers with %
 
-local lsp_signature_config = {
-  bind = true,
-  hint_enable = false, -- disable virtual text
-  hint_prefix = "", -- remove panda icon from virtual text
-  handler_opts = { border = "single" }, -- show current argument in the popup
-}
+-- local lsp_signature_config = {
+--   bind = true,
+--   hint_enable = false, -- disable virtual text
+--   hint_prefix = "", -- remove panda icon from virtual text
+--   handler_opts = { border = "single" }, -- show current argument in the popup
+-- }
 
 lvim.plugins = {
   { "vimwiki/vimwiki" }, -- note taking plugin
   { "vim-test/vim-test" }, -- unit tests
   { "nvim-treesitter/nvim-treesitter-context" }, -- sticky scroll context
-
   { "AndrewRadev/splitjoin.vim" }, -- gJ, gS movements
   { "christoomey/vim-titlecase" }, -- "gz" movement to toggle the words case
   { "christoomey/vim-tmux-navigator" }, -- easier tmux navigation
   { "felipec/vim-sanegx", event = "BufRead" }, -- go to the url using gX
-
   {
     "tiagovla/scope.nvim",
     config = function()
       require("scope").setup()
     end,
   }, -- better bufferline (sort buffers into tabs)
-
   { "christianchiarulli/nvcode-color-schemes.vim" }, -- a bunch of generated thees
-
   { "tpope/vim-repeat" }, -- better "."
   { "tpope/vim-sleuth" }, -- detect the relevant tabs / shifts
   { "tpope/vim-surround" }, -- surround movement
@@ -36,8 +32,6 @@ lvim.plugins = {
   { "tpope/vim-obsession" }, -- save the session
   { "tpope/vim-unimpaired" }, -- additional mappings
   -- { "tpope/vim-vinegar" }, -- better netrw
-  -- { "tpope/vim-eunuch" }, -- UNIX commands
-
   {
     "andymass/vim-matchup", -- better "%"
     requires = "leafOfTree/vim-vue-plugin", -- vue syntax highlighting and % fix
@@ -108,13 +102,6 @@ lvim.plugins = {
       }
     end,
   },
-  -- WARNING: this plugin doesn't work as of 31.07.2022
-  -- {
-  --   "Pocco81/auto-save.nvim", -- autosave
-  --   config = function()
-  --     require("autosave").setup()
-  --   end,
-  -- },
   {
     "folke/todo-comments.nvim", -- highlight the todo, hack, note, etc... comments
     event = "BufRead",
@@ -137,19 +124,26 @@ lvim.plugins = {
     event = "InsertEnter",
   },
   {
-    "ray-x/lsp_signature.nvim", -- show function arguments as you type
-    config = function()
-      require("lsp_signature").setup(lsp_signature_config)
-    end,
-    event = "BufRead",
-  },
-  {
     "windwp/nvim-ts-autotag", -- complete or replace the open & closing tags
     event = "InsertEnter",
     config = function()
       require("nvim-ts-autotag").setup()
     end,
   },
+  -- WARNING: this plugin doesn't work as of 31.07.2022
+  -- {
+  --   "Pocco81/auto-save.nvim", -- autosave
+  --   config = function()
+  --     require("autosave").setup()
+  --   end,
+  -- },
+  -- {
+  --   "ray-x/lsp_signature.nvim", -- show function arguments as you type
+  --   config = function()
+  --     require("lsp_signature").setup(lsp_signature_config)
+  --   end,
+  --   event = "BufRead",
+  -- },
   -- {
   --   "iamcco/markdown-preview.nvim", -- localhost markdown server
   --   run = "cd app && yarn install",
@@ -170,4 +164,4 @@ lvim.plugins = {
 }
 
 -- HACK: requiring lsp_signature in packer config function doesn't work as of 18.05.2022
-require("lsp_signature").setup(lsp_signature_config)
+-- require("lsp_signature").setup(lsp_signature_config)
