149da5871493bee31ba1a85b1f817051cc6c13fc
Jakub Reroń <kubareron@gmail.com>
2022-09-26 11:44:58 +0200

    config(builtin): ⚙️ auto-commit changes

--- lua/config/builtin.lua
+++ lua/config/builtin.lua
@@ -1,5 +1,6 @@
 lvim.builtin.terminal.active = true
 lvim.builtin.breadcrumbs.active = true
+lvim.builtin.illuminate.active = false
 
 lvim.builtin.bufferline.options.numbers = "buffer_id"
 lvim.builtin.bufferline.options.always_show_bufferline = true
