ff9b2e6b706703503eac952b8781497de0e57bf9
Jakub Reroń <kubareron@gmail.com>
2022-09-28 22:11:23 +0200

    config(plugins): ⚙️ auto-commit changes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -40,14 +40,22 @@ lvim.plugins = {
   {
     "norcalli/nvim-colorizer.lua", -- highlight the hex / rgb colors
     config = function()
-      require("plugin.colorizer").config()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
     end,
   },
   {
     "windwp/nvim-spectre", -- search & replace throughout all the files (without vimgrepping)
     event = "BufRead",
     config = function()
-      require("plugin.spectre").config()
+      require("spectre").setup()
     end,
   },
   {
