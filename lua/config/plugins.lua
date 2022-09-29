9f972f83741dde8bf032963896d0da8912a34651
Jakub Reroń <kubareron@gmail.com>
2022-09-29 13:36:24 +0200

    config(plugins): ⚙️ auto-commit changes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -98,20 +98,20 @@ lvim.plugins = {
     end,
     requires = { "plenary.nvim", "toggleterm.nvim" },
   },
-  -- {
-  --   "tzachar/cmp-tabnine", -- AI autocompletion
-  --   config = function()
-  --     local tabnine = require "cmp_tabnine.config"
-  --     tabnine:setup {
-  --       max_lines = 1000,
-  --       max_num_results = 20,
-  --       sort = true,
-  --     }
-  --   end,
-  --   run = "./install.sh",
-  --   requires = "hrsh7th/nvim-cmp",
-  --   event = "InsertEnter",
-  -- },
+  {
+    "tzachar/cmp-tabnine", -- AI autocompletion
+    config = function()
+      local tabnine = require "cmp_tabnine.config"
+      tabnine:setup {
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+      }
+    end,
+    run = "./install.sh",
+    requires = "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+  },
   {
     "Pocco81/auto-save.nvim",
     config = function()
