6681cc532fe1b9ecdb615788eb5ee66ac1d89e34
Jakub Reroń <kubareron@gmail.com>
2022-10-06 16:36:25 +0200

    config(general): don't show close icons in bufferline

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -8,6 +8,7 @@ lvim.builtin.notify.active = false
 
 lvim.builtin.bufferline.options.numbers = "buffer_id"
 lvim.builtin.bufferline.options.always_show_bufferline = true
+lvim.builtin.bufferline.options.show_close_icon = false
 
 lvim.builtin.which_key.setup.plugins.marks = true
 lvim.builtin.which_key.setup.plugins.registers = true
