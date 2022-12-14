35163594dd7ccd8e7cdb58e5961f9fcb032252ac
Jakub Reroń <kubareron@gmail.com>
2022-12-14 13:11:14 +0100

    config(plugins/git-conflict): disable default commands

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -41,7 +41,7 @@ lvim.plugins = {
     config = function()
       require("git-conflict").setup {
         default_mappings = true, -- disable buffer local mapping created by this plugin
-        default_commands = true, -- disable commands created by this plugin
+        default_commands = false, -- disable commands created by this plugin
         disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
         highlights = { -- They must have background color, otherwise the default color will be used
           incoming = "DiffText",
