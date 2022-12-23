980faedc5638994206db06f904638a1c0c86fd95
Jakub Reroń <kubareron@gmail.com>
2022-12-23 13:16:15 +0100

    chore(plugins): colorizer

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -36,6 +36,20 @@ lvim.plugins = {
     "nvim-treesitter/nvim-treesitter-context",
   }, -- sticky scroll context
   {
+    "norcalli/nvim-colorizer.lua", -- highlight the hex / rgb colors
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        -- css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        -- css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+  {
     "akinsho/git-conflict.nvim",
     tag = "*",
     config = function()
