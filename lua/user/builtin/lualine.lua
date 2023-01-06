2754b7946758f38880c954539f0267d0b65966df
Jakub Reroń <kubareron@gmail.com>
2023-01-06 13:03:32 +0100

    config(vim-obsession): lualine tracking-session icon

--- lua/user/builtin/lualine.lua
+++ lua/user/builtin/lualine.lua
@@ -1,12 +1,17 @@
 local components = require "lvim.core.lualine.components"
-local function packageinfo()
+local function package_info()
   return require("package-info").get_status()
 end
 
+local function obsession_status()
+  return '%{ObsessionStatus("", "")}'
+end
+
 lvim.builtin.lualine.sections.lualine_c = {
+  obsession_status,
   components.filename,
   components.diff,
   components.python_env,
-  packageinfo,
+  package_info,
 }
 lvim.builtin.lualine.inactive_sections.lualine_c = lvim.builtin.lualine.sections.lualine_c
