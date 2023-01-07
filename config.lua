3d3106740a0b20caf762f01dc08f083bb87fd539
Jakub Reroń <kubareron@gmail.com>
2023-01-07 22:10:51 +0100

    fix(init.lua): reorder imports

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 lvim.format_on_save = false
 lvim.reload_config_on_save = false
 
+require "user.plugins"
+
 require "user.shared"
 
 require "user.builtin"
@@ -8,5 +10,3 @@ require "user.builtin"
 require "user.keymaps"
 
 require "user.lsp"
-
-require "user.plugins"
