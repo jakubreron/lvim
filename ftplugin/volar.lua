2d9da2028e9592851c6434371289c3b7a7b9f68c
Jakub Reroń <kubareron@gmail.com>
2023-01-10 11:08:07 +0100

    config(ftplugin/volar): try to setup volar

---
+++ ftplugin/volar.lua
@@ -0,0 +1,27 @@
+local util = require "lspconfig.util"
+local function get_typescript_server_path(root_dir)
+  local global_ts = "~/.local/share/yarn/global/node_modules/typescript/lib"
+  -- Alternative location if installed as root:
+  -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+  local found_ts = ""
+  local function check_dir(path)
+    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+require("lvim.lsp.manager").setup("volar", {
+  -- NOTE: look for local typescript, if it does not exists, look for global one
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
+  -- NOTE: enable takeover mode <https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar>
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+})
