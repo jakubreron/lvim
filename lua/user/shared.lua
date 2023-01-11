2710a1010dbf0052cd39c8393524bfa831cd7c4a
Jakub Reroń <kubareron@gmail.com>
2023-01-11 17:05:10 +0100

    refactor: starts autocmds migration to lua

--- lua/user/shared.lua
+++ lua/user/shared.lua
@@ -1,7 +1,9 @@
+-- TODO: migrate all autocmds to lua
 vim.cmd [[
   source ~/.config/nvim/shared/general.lua
   source ~/.config/nvim/shared/mappings.vim
   source ~/.config/nvim/shared/shortcuts.vim
   source ~/.config/nvim/shared/autocmds.vim
+  source ~/.config/nvim/shared/autocmds.lua
   source ~/.config/nvim/shared/netrw.vim
 ]]
