6fff2e036168837e434e52a4657b7cbc2bff5c28
Jakub Reroń <kubareron@gmail.com>
2023-01-11 20:48:15 +0100

    config(tabnine): better settings

--- lua/user/plugins/settings/tabnine.lua
+++ lua/user/plugins/settings/tabnine.lua
@@ -8,8 +8,16 @@ M.config = function()
 
   tabnine.setup {
     max_lines = 1000,
-    max_num_results = 20,
+    max_num_results = 10,
     sort = true,
+    run_on_every_keystroke = true,
+    snippet_placeholder = "..",
+    ignored_file_types = {
+      -- default is not to ignore
+      -- uncomment to ignore in lua:
+      -- lua = true
+    },
+    show_prediction_strength = true,
   }
 end
 
