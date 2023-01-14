8b32751854d32750f370ed0b2a13808adcaef8e1
Jakub Reroń <kubareron@gmail.com>
2023-01-14 20:26:09 +0100

    config: more treesitter textobjects

--- lua/user/builtin/treesitter.lua
+++ lua/user/builtin/treesitter.lua
@@ -34,8 +34,17 @@ lvim.builtin.treesitter.textobjects = {
     keymaps = {
       ["af"] = "@function.outer",
       ["if"] = "@function.inner",
-      ["ac"] = "@class.outer",
-      ["ic"] = "@class.inner",
+
+			["ac"] = "@call.outer",
+			["ic"] = "@call.inner",
+			["aa"] = "@parameter.outer",
+			["ia"] = "@parameter.inner",
+			["al"] = "@loop.outer",
+			["il"] = "@loop.inner",
+			["ai"] = "@conditional.outer",
+			["ii"] = "@conditional.inner",
+			["a/"] = "@comment.outer",
+			["i/"] = "@comment.inner",
     },
   },
   move = {
