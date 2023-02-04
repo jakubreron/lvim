14030cd8bf20d12cce3950a16531c43398199da4
Jakub Reroń <kubareron@gmail.com>
2023-02-04 22:31:27 +0100

    fix(nvimtree): remove deprecated option

--- lua/user/builtin/nvimtree.lua
+++ lua/user/builtin/nvimtree.lua
@@ -5,5 +5,4 @@ lvim.builtin.nvimtree.setup.renderer.indent_markers.enable = true
 
 lvim.builtin.nvimtree.setup.filters.custom = {}
 lvim.builtin.nvimtree.setup.disable_netrw = true
-lvim.builtin.nvimtree.setup.open_on_setup = true
 lvim.builtin.nvimtree.setup.auto_reload_on_write = true
