d130fa3ec4eac31a791a8f580091b7438f7028d2
Jakub Reroń <kubareron@gmail.com>
2023-02-18 00:28:17 +0100

    refactor: leaves only plugin-related keybinds

--- lua/user/keymaps/init.lua
+++ lua/user/keymaps/init.lua
@@ -7,27 +7,13 @@ lvim.keys = {
   normal_mode = {
     ["<C-f>"] = harpoon_ui.toggle_quick_menu,
     ["<C-b>"] = harpoon_mark.add_file,
-
     -- ["]c"] = harpoon_ui.nav_next,
     -- ["[c"] = harpoon_ui.nav_prev,
-
-    ["]x"] = "<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>", -- next diagnostic
-    ["[x"] = "<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>", -- prev diagnostic
 
     ["]c"] = "<cmd>lua require'gitsigns'.next_hunk({navigation_message = false})<CR>", -- next change
     ["[c"] = "<cmd>lua require'gitsigns'.prev_hunk({navigation_message = false})<CR>", -- prev change
 
     ["]u"] = "<cmd>lua require('neotest').jump.next({ status = 'failed' })<CR>", -- next failed test
     ["[u"] = "<cmd>lua require('neotest').jump.prev({ status = 'failed' })<CR>", -- prev failed test
-
-    ["yoea"] = "<cmd>set eventignore=all<CR>",
-    ["yoen"] = "<cmd>set eventignore=<CR>",
-    ["yoss"] = "<cmd>setlocal spell!<CR>",
-    ["yose"] = "<cmd>setlocal spell! spelllang=en<CR>",
-    ["yosp"] = "<cmd>setlocal spell! spelllang=pl<CR>",
-  },
-
-  visual_mode = {
-    ["<leader>p"] = '"_dP',
   },
 }
