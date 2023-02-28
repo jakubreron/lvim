030f305dc7a79fa374d03023ec4a175788653034
Jakub Reroń <kubareron@gmail.com>
2023-02-28 12:59:08 +0100

    config(nvimtree): numbers + adaptive_size

--- lua/user/builtin/nvimtree.lua
+++ lua/user/builtin/nvimtree.lua
@@ -1,6 +1,10 @@
 lvim.builtin.nvimtree.setup.renderer.full_name = true
 lvim.builtin.nvimtree.setup.renderer.indent_markers.enable = true
 
+lvim.builtin.nvimtree.setup.view.number = true
+lvim.builtin.nvimtree.setup.view.relativenumber = true
+lvim.builtin.nvimtree.setup.view.adaptive_size = true
+
 lvim.builtin.nvimtree.setup.filters.custom = {}
 lvim.builtin.nvimtree.setup.disable_netrw = true
 lvim.builtin.nvimtree.setup.auto_reload_on_write = true
