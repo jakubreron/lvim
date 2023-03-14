86eca09c45a0207c53beb9a5e63c082e8ed9638e
Jakub Reroń <kubareron@gmail.com>
2023-03-14 15:33:52 +0100

    config(nvimtree): disable adaptive size

--- lua/user/builtin/nvimtree.lua
+++ lua/user/builtin/nvimtree.lua
@@ -3,7 +3,7 @@ lvim.builtin.nvimtree.setup.renderer.indent_markers.enable = true
 
 lvim.builtin.nvimtree.setup.view.number = true
 lvim.builtin.nvimtree.setup.view.relativenumber = true
-lvim.builtin.nvimtree.setup.view.adaptive_size = true
+-- lvim.builtin.nvimtree.setup.view.adaptive_size = true
 
 lvim.builtin.nvimtree.setup.filters.custom = {}
 lvim.builtin.nvimtree.setup.disable_netrw = true
