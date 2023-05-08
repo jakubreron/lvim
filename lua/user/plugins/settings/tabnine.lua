fbd0d8551e115ee16ba868611a1eba001fa6ddbb
Jakub Reroń <kubareron@gmail.com>
2023-05-08 13:58:18 +0200

    fix(tabnine): comment out setup for now

--- lua/user/plugins/settings/tabnine.lua
+++ lua/user/plugins/settings/tabnine.lua
@@ -6,19 +6,19 @@ M.config = function()
     return
   end
 
-  tabnine.setup {
-    max_lines = 1000,
-    max_num_results = 10,
-    sort = true,
-    run_on_every_keystroke = true,
-    snippet_placeholder = "..",
-    ignored_file_types = {
-      -- default is not to ignore
-      -- uncomment to ignore in lua:
-      -- lua = true
-    },
-    show_prediction_strength = true,
-  }
+  -- tabnine.setup {
+  --   max_lines = 1000,
+  --   max_num_results = 10,
+  --   sort = true,
+  --   run_on_every_keystroke = true,
+  --   snippet_placeholder = "..",
+  --   ignored_file_types = {
+  --     -- default is not to ignore
+  --     -- uncomment to ignore in lua:
+  --     -- lua = true
+  --   },
+  --   show_prediction_strength = true,
+  -- }
 end
 
 return M
