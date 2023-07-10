b791e77ca8af9b822cf5131ff6d351a1c670d6d3
Jakub Reroń <kubareron@gmail.com>
2023-07-10 13:29:29 +0200

    fix: mappings.lua -> keymaps.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
   source ~/.config/nvim/shared/general.lua
   source ~/.config/nvim/shared/autocmds.lua
-  source ~/.config/nvim/shared/mappings.lua
+  source ~/.config/nvim/shared/keymaps.lua
   source ~/.config/nvim/shared/netrw.lua
   source ~/.config/nvim/shared/shortcuts.vim
 ]]
