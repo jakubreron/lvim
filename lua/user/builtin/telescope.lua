aaf06dfc8c160f965868a89dd9c18a1629df2c0f
Jakub Reroń <kubareron@gmail.com>
2023-08-21 23:35:05 +0200

    refactor(builtin/telescope): uses builtin telescope instead of requiring another one

--- lua/user/builtin/telescope.lua
+++ lua/user/builtin/telescope.lua
@@ -1,19 +1,20 @@
-lvim.builtin.telescope.theme = "ivy"
-
-local telescope = require "telescope"
 local lga_actions = require "telescope-live-grep-args.actions"
 
--- TODO: check if there is a way with reusing the lvim.builtin object
-telescope.setup {
-  extensions = {
-    live_grep_args = {
-      auto_quoting = true,
-      mappings = {
-        i = {
-          ["<C-q>"] = lga_actions.quote_prompt(),
-          ["<C-g>"] = lga_actions.quote_prompt { postfix = " --iglob " },
-          ["<C-s>"] = lga_actions.quote_prompt { postfix = " --iglob *.spec.js" },
-        },
+lvim.builtin.telescope.theme = "ivy"
+lvim.builtin.telescope.extensions = {
+  fzf = {
+    fuzzy = false, -- false will only do exact matching
+    override_generic_sorter = true, -- override the generic sorter
+    override_file_sorter = true, -- override the file sorter
+    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+  },
+  live_grep_args = {
+    auto_quoting = true,
+    mappings = {
+      i = {
+        ["<C-q>"] = lga_actions.quote_prompt(),
+        ["<C-g>"] = lga_actions.quote_prompt { postfix = " --iglob " },
+        ["<C-s>"] = lga_actions.quote_prompt { postfix = " --iglob *.spec.js" },
       },
     },
   },
