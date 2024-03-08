d89aeb3648ecc843cc1599465d2a08ca96a9fe35
Jakub Reroń <kubareron@gmail.com>
2024-03-08 14:48:40 +0900

    refactor(plugins): better defaults

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,9 +1,20 @@
 lvim.plugins = {
   { "vimwiki/vimwiki" },
+
   { "AndrewRadev/splitjoin.vim" }, -- gJ, gS movements
+
   { "szw/vim-maximizer" }, -- maximize current window
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "stevearc/dressing.nvim" }, -- better default nvim interfaces
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+  }, -- better default nvim interfaces
   -- {
   --   "folke/noice.nvim",
   --   event = "VeryLazy",
@@ -19,26 +30,52 @@ lvim.plugins = {
   --     -- "rcarriga/nvim-notify",
   --   }
   -- },
-  { "folke/todo-comments.nvim", config = true }, -- todo comments highlight
-  { "ThePrimeagen/harpoon", lazy = true }, -- better file navigation since bufferline is disabled
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  }, -- todo comments highlight
+
+  {
+    "ThePrimeagen/harpoon",
+    lazy = true,
+  }, -- better file navigation since bufferline is disabled
+
   { "nvim-treesitter/nvim-treesitter-context" }, -- sticky scroll context
   -- { "nvim-treesitter/nvim-treesitter-textobjects" }, -- more movements (if, af, ic, ac, etc...)
+
   { "christoomey/vim-titlecase" }, -- "gz" movement to toggle the words case
-  { "christoomey/vim-tmux-navigator" }, -- tmux navigation from within nvim
+
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }, -- tmux navigation from within nvim
+
   { "tpope/vim-repeat" }, -- better "."
   { "tpope/vim-surround" }, -- surround movement
   { "tpope/vim-obsession" }, -- save the session
   { "tpope/vim-unimpaired" }, -- additional mappings
+
   {
     "nvim-neotest/neotest", -- run tests directly from the file
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      {
-        "haydenmeade/neotest-jest",
-        commit = "c2118446d770fedb360a91b1d91a7025db86d4f1",
-        pin = true,
-      },
+      "haydenmeade/neotest-jest",
     },
     config = function()
       require("user.plugins.settings.neotest").config()
@@ -46,9 +83,11 @@ lvim.plugins = {
     event = "BufWinEnter *.spec.*",
     lazy = true,
   },
+
   -- {
   --   "mxsdev/nvim-dap-vscode-js" -- debugger adapter
   -- },
+
   {
     "norcalli/nvim-colorizer.lua", -- highlight the hex / rgb colors
     config = function()
@@ -60,6 +99,7 @@ lvim.plugins = {
     },
     lazy = true,
   },
+
   -- {
   --   "akinsho/git-conflict.nvim",
   --   version = "*",
@@ -67,34 +107,43 @@ lvim.plugins = {
   --     require("user.plugins.settings.git-conflict").config()
   --   end,
   -- }, -- easier conflicts
+
   {
     "windwp/nvim-spectre", -- search & replace throughout all the files (without vimgrepping)
     event = "BufRead",
     config = true,
     lazy = true,
   },
+
   {
     "vuki656/package-info.nvim", -- check if the package info is up to date
     dependencies = "MunifTanjim/nui.nvim",
-    config = true,
     event = "BufWinEnter package.json",
+    config = true,
     lazy = true,
   },
+
   {
     "kevinhwang91/nvim-bqf", -- better quickfix window (preview, search & replace, etc...)
     event = { "BufRead", "BufNew" },
     config = true,
     lazy = true,
   },
+
   {
     "nvim-telescope/telescope-live-grep-args.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
   },
+
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = {
+      "typescript",
+      "typescriptreact",
+    },
     opts = {},
   },
 
