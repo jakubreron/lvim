9d0f68c56ccfff42f562200331fb7f392de406c8
Jakub Reroń <kubareron@gmail.com>
2024-03-21 16:29:04 +0900

    config(plugins): add auto-save fork

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -180,6 +180,13 @@ lvim.plugins = {
     opts = {},
   },
 
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {},
+  },
+
   -- {
   --   "simrat39/rust-tools.nvim",
   --   config = function()
