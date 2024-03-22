8f9855372ed7cc03354eafcd6e31d16429e31052
Jakub Reroń <kubareron@gmail.com>
2024-03-23 00:48:27 +0900

    config(nvimtree): disable netrw

--- lua/user/builtin/nvimtree.lua
+++ lua/user/builtin/nvimtree.lua
@@ -7,7 +7,6 @@ lvim.builtin.nvimtree.setup.view.relativenumber = true
 lvim.builtin.nvimtree.setup.view.adaptive_size = true
 
 lvim.builtin.nvimtree.setup.filters.custom = {}
-lvim.builtin.nvimtree.setup.disable_netrw = false
+lvim.builtin.nvimtree.setup.disable_netrw = true
 lvim.builtin.nvimtree.setup.auto_reload_on_write = true
-
 lvim.builtin.nvimtree.setup.update_cwd = false
