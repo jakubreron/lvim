70aa9ae7de0a94fe3080392c0168e769e5859065
Jakub Reroń <kubareron@gmail.com>
2024-03-25 17:08:47 +0900

    fix(harpoon2): saving on toggle

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -63,6 +63,15 @@ lvim.plugins = {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+        key = function()
+          return vim.loop.cwd()
+        end,
+      },
+    },
   },
   {
     "letieu/harpoon-lualine",
