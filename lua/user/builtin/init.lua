f81dcf02d6457fe835cd9b755d572f881250f88c
Jakub Reroń <kubareron@gmail.com>
2024-03-25 16:54:20 +0900

    perf(bigfile): disable certain features on big file

--- lua/user/builtin/init.lua
+++ lua/user/builtin/init.lua
@@ -24,27 +24,26 @@ lvim.builtin.lir.active = false
 lvim.builtin.dap.active = false
 lvim.builtin.breadcrumbs.active = false
 
--- lvim.builtin.bigfile.config = {
---   filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
---   pattern = function(bufnr, filesize_mib)
---     -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
---     local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
---     local file_length = #file_contents
---     if file_length > 5000 or filesize_mib > 0.5 then
---       return true
---     end
---   end,
---   features = { -- features to disable
---     "indent_blankline",
---     "illuminate",
---     "lsp",
---     "treesitter",
---     "syntax",
---     "matchparen",
---     "vimopts",
---     "filetype",
---   },
--- }
+lvim.builtin.bigfile.config = {
+  filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+  pattern = function(bufnr, filesize_mib)
+    -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+    local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+    local file_length = #file_contents
+    if file_length > 5000 or filesize_mib > 0.5 then
+      return true
+    end
+  end,
+  features = { -- features to disable
+    "indent_blankline",
+    "illuminate",
+    -- "lsp",
+    -- "treesitter",
+    "syntax",
+    "vimopts",
+    -- "filetype",
+  },
+}
 
 lvim.builtin.indentlines.active = true
 lvim.builtin.autopairs.active = true
