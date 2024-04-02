7ec8bfc7cf536bb057108bbcb2e5ff391d4bbbd2
Jakub Reroń <kubareron@gmail.com>
2024-04-02 18:55:49 +0900

    fix: imports

--- lua/user/keymaps/init.lua
+++ lua/user/keymaps/init.lua
@@ -1,43 +1,5 @@
 require "user.keymaps.which-key"
 
--- local harpoon = require "harpoon"
--- -- REQUIRED
--- harpoon:setup()
--- -- REQUIRED
-
--- vim.keymap.set("n", "<C-f>", function()
---   harpoon.ui:toggle_quick_menu(harpoon:list())
--- end, {
---   desc = "Toggle harpoon quick menu",
--- })
-
--- vim.keymap.set("n", "<C-b>", function()
---   harpoon:list():append()
--- end, {
---   desc = "Add file to harpoon",
--- })
-
--- vim.keymap.set("n", "]h", function()
---   harpoon:list():next()
--- end, {
---   desc = "Next harpoon file",
--- })
-
--- vim.keymap.set("n", "[h", function()
---   harpoon:list():prev()
--- end, {
---   desc = "Prev harpoon file",
--- })
-
--- for i = 1, 6 do
---   lvim.builtin.which_key.mappings[tostring(i)] = {
---     function()
---       harpoon:list():select(i)
---     end,
---     "Mark " .. i,
---   }
--- end
-
 -- TODO: if project has git, use next/prev git change, if not, then use changelist
 vim.keymap.set("n", "]c", "<cmd>lua require'gitsigns'.next_hunk({navigation_message = false})<CR>", {
   desc = "Next git change",
@@ -45,3 +7,6 @@ vim.keymap.set("n", "]c", "<cmd>lua require'gitsigns'.next_hunk({navigation_mess
 vim.keymap.set("n", "[c", "<cmd>lua require'gitsigns'.prev_hunk({navigation_message = false})<CR>", {
   desc = "Prev git change",
 })
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
